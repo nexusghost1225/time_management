d76d62e953a753e77ed04d749e7277388aa31d09
nexusghost1225 <JGBxmas2000@hotmail.com>
2022-11-18 21:05:15 -0500

    i tried

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    document.getElementsByClassName("saveBtn").addEventListener("click", console.log(save););
-if (timeblock hour < current hour) {color gray } 
-else if (timeblock hour === current hour) {color red } 
-else if (timeblock hour > current hour) { color green } 
-    
-}
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-var AdvancedFormat = require('dayjs/plugin/advancedFormat')
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  }); import * as dayjs from 'dayjs'
-dayjs().format()
+var now = dayjs()
+console.log(now);
+
+var d = new Date()
+var day = dayjs(d)
+
+if (timeblock hour < current hour) {color grey } {
+
+} else if (timeblock hour === current hour) {color red } {
+
+}else if (timeblock hour > current hour) {color green }
